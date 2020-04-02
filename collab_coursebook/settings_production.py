208ab370ac082d2e6f4fee4ee280d24b5732a25f
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2020-04-02 21:31:55 +0200

    Add production settings and secrets sample

---
+++ collab_coursebook/settings_production.py
@@ -0,0 +1,35 @@
+"""
+This is the settings file used in production.
+First, it imports all default settings, then overrides respective ones.
+Secrets are stored in and imported from an additional file, not set under version control.
+"""
+
+import collab_coursebook.settings_secrets as secrets
+
+# noinspection PyUnresolvedReferences
+from AKPlanning.settings import *
+
+### SECURITY ###
+
+DEBUG = False
+
+ALLOWED_HOSTS = secrets.HOSTS
+
+SECRET_KEY = secrets.SECRET_KEY
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+### DATABASE ###
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': secrets.DB_NAME,
+        'USER': secrets.DB_USER,
+        'PASSWORD': secrets.DB_PASSWORD,
+    }
+}
+
+# TODO: caching
