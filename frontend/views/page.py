4507665d9cc27acb0ce03a435516cd2cc2d2a426
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2020-04-08 14:58:18 +0200

    First version of dashboard
    Implement overview over courses, categories and periods on dashboard
    Introduce cards for categories and periods
    Adapt index and navbar
    Add link to all courses overview
    Use different placeholder colors for cards

--- frontend/views/page.py
+++ frontend/views/page.py
@@ -1,9 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+
+from base.models import Period, Category
 
 
 class StartView(TemplateView):
     template_name = "frontend/index.html"
 
 
-class DashboardView(TemplateView):
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "frontend/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["periods"] = Period.objects.all()
+        ctx["categories"] = Category.objects.all()
+        return ctx
+
