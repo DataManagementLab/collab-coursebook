0bc665359349221e725ac55b777599158752e5ba
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2020-06-12 14:20:10 +0200

    Remove obsolete import

--- frontend/views/profile.py
+++ frontend/views/profile.py
@@ -1,4 +1,3 @@
-from dal import autocomplete
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
