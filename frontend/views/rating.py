9f25bb7e860b15a0e2ee89fe790fd2093915bbce
leonard.bongard <leo.bongard@gmx.de>
2020-08-11 16:52:55 +0200

    remove unnecessary file

--- frontend/views/rating.py
+++
@@ -1,8 +0,0 @@
-
-from django.views.generic import UpdateView
-
-from base.models import Content, Comment, Course, Topic, Favorite, Rating
-from frontend.forms import CommentForm, TranslateForm
-
-class ContentRatingView(UpdateView):
-    None
