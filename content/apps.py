c15cdecd5ae079d284826d9be4e639d3715eb64b
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-01-10 17:22:27 +0100

    Update apps.py

--- content/apps.py
+++ content/apps.py
@@ -8,7 +8,7 @@ from django.apps import AppConfig
 class ContenttypesConfig(AppConfig):
     """ Content types configuration
 
-    Configures the pluggable application content types.
+    Configures the pluggable application for the content types.
 
     Attributes:
         ContenttypesConfig.name (str): Defines which application the configuration applies to
