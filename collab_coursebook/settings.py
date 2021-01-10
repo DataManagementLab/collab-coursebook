490c7fd20ee88b2c433e000eafd156dd8efc54e7
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-01-10 17:09:17 +0100

    Update clooab_coursebook direcotry
    Reformat code

--- collab_coursebook/settings.py
+++ collab_coursebook/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from django.utils.translation import gettext_lazy as _
 
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,7 +30,6 @@ SECRET_KEY = '_-xt(rpt+&&-c_^5h1bxgx(n63uctron!gi*k)*=4$rgu@nfs!'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -83,7 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'collab_coursebook.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -93,7 +91,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -143,7 +140,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- collab_coursebook/urls.py
+++ collab_coursebook/urls.py
@@ -13,21 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 import django_cas_ng.views
+
 import debug_toolbar
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('accounts/login/', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
-    path('accounts/logout/', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
-    path('accounts/callback/', django_cas_ng.views.CallbackView.as_view(), name='cas_ng_proxy_callback'),
-    path('', include('frontend.urls', namespace='frontend')),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('__debug__/', include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('i18n/', include('django.conf.urls.i18n')),
+                  path('accounts/login/', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
+                  path('accounts/logout/', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
+                  path('accounts/callback/', django_cas_ng.views.CallbackView.as_view(), name='cas_ng_proxy_callback'),
+                  path('', include('frontend.urls', namespace='frontend')),
+                  path('i18n/', include('django.conf.urls.i18n')),
+                  path('__debug__/', include(debug_toolbar.urls)),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
