dfbfdd8aa5348520e72196584b92b462b220b886
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-01-11 21:43:33 +0100

    Update views.py

--- export/views.py
+++ export/views.py
@@ -1,3 +1,8 @@
+"""Purpose of this file
+
+This file contains functions related to generating views.
+"""
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -6,7 +11,19 @@ from export.helper_functions import LaTeX
 
 
 def generate_coursebook(request, pk, template="content/export/base.tex", context=None):
-    """ Generates a PDF file with nametags for students in the queryset"""
+    """Generate course book
+
+    Generates a PDF file with name tags for students in the queryset.
+
+    Parameters:
+        request (WSGIRequest): TODO
+        pk (TODO):
+        template (str): The template latex path
+        context (dict): The context of the content
+
+    return: the PDF, PDF LaTeX output and the rendered template
+    rtype: Tuple[bytes, Tuple[bytes, bytes], str]
+    """
 
     if context is None:
         context = {}
@@ -24,7 +41,18 @@ def generate_coursebook(request, pk, template="content/export/base.tex", context
 
 
 def generate_coursebook_response(request, pk, filename='coursebook.pdf'):
-    """ Generates a PDF file with nametags for students in the queryset and sends it to the browser"""
+    """Generate course book response
+
+    Generates a PDF file with name tags for students in the queryset and sends it to the browser.
+
+    Parameters:
+        request (WSGIRequest): TODO
+        pk (TODO):
+        filename (str): The name of the file
+
+    return: the http response of the generated PDF file
+    rtype: HttpResponse
+    """
 
     # Call the method for coursebook generation and write the output afterwards
     (pdf, pdflatex_output, tex_template) = generate_coursebook(request, pk)
@@ -32,8 +60,21 @@ def generate_coursebook_response(request, pk, filename='coursebook.pdf'):
 
 
 def write_response(request, pdf, pdflatex_output, tex_template, filename, content_type='application/pdf'):
+    """Write response
+
+    Renders a pdf and sends it to the browser.
+
+    Parameters:
+        request (WSGIRequest): TODO
+        pdf : The PDF
+        pdflatex_output : PDF LaTeX output
+        tex_template (str): The rendered template
+        filename (str): The name of the file
+        content_type (str): The type of the content
+    """
     if not pdf:
-        return render(request, "frontend/coursebook/rendering-error.html", {"content": pdflatex_output[0].decode("utf-8"), "tex_template": tex_template.decode("utf-8")})
+        return render(request, "frontend/coursebook/rendering-error.html",
+                      {"content": pdflatex_output[0].decode("utf-8"), "tex_template": tex_template.decode("utf-8")})
     response = HttpResponse(content_type=content_type)
     response['Content-Disposition'] = 'attachment; filename=' + filename
     response.write(pdf)
@@ -41,8 +82,20 @@ def write_response(request, pdf, pdflatex_output, tex_template, filename, conten
 
 
 def generate_pdf(user, course, content, template="content/export/base.tex", context=None):
-    """ Generates a PDF file with nametags for students in the queryset"""
+    """Generate PDF
 
+    Generates a PDF file with name tags for students in the queryset.
+
+    Parameters:
+        user (User): The user of the content
+        course (Course): The course where the content belongs to
+        content (dict): The content
+        template (str): The path of the LaTeX template
+        context (dict): The context of the content
+
+    return: the PDF, PDF LaTeX output and the rendered template
+    rtype: Tuple[bytes, Tuple[bytes, bytes], str]
+    """
     if context is None:
         context = {}
 
@@ -51,14 +104,25 @@ def generate_pdf(user, course, content, template="content/export/base.tex", cont
     context['course'] = course
     context['contents'] = [content]
 
-    # Perform compilation given context and template
+    # Performs compilation given context and template
     (pdf, pdflatex_output, tex_template) = LaTeX.render(context, template, [])
     return pdf, pdflatex_output, tex_template
 
 
 def generate_pdf_response(user, course, content):
-    """ Generates a PDF file with nametags for students in the queryset """
+    """Generate pdf response
 
-    # call the method for generating the pdf and return the pdf
+    Generates a PDF file with name tags for students in the queryset.
+
+    Parameters:
+        user (User): The user of the content
+        course (Course): The course where the content belongs to
+        content (dict): The content
+
+    return: the generated PDF
+    rtype: bytes
+    """
+
+    # Calls the function for generating the pdf and return the pdf
     (pdf, pdflatex_output, tex_template) = generate_pdf(user, course, content)
     return pdf
