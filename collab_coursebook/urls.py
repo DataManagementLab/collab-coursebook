055c287e91b4ad2466f1d047dc30f3be669f2486
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-01-18 14:32:00 +0100

    PyLint collab_coursebook
    PyLint fixed most warnings

--- collab_coursebook/urls.py
+++ collab_coursebook/urls.py
@@ -24,12 +24,24 @@ import django_cas_ng.views
 import debug_toolbar
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('i18n/', include('django.conf.urls.i18n')),
-                  path('accounts/login/', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
-                  path('accounts/logout/', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
-                  path('accounts/callback/', django_cas_ng.views.CallbackView.as_view(), name='cas_ng_proxy_callback'),
-                  path('', include('frontend.urls', namespace='frontend')),
-                  path('i18n/', include('django.conf.urls.i18n')),
-                  path('__debug__/', include(debug_toolbar.urls)),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/',
+                       admin.site.urls),
+                  path('i18n/',
+                       include('django.conf.urls.i18n')),
+                  path('accounts/login/',
+                       django_cas_ng.views.LoginView.as_view(),
+                       name='cas_ng_login'),
+                  path('accounts/logout/',
+                       django_cas_ng.views.LogoutView.as_view(),
+                       name='cas_ng_logout'),
+                  path('accounts/callback/',
+                       django_cas_ng.views.CallbackView.as_view(),
+                       name='cas_ng_proxy_callback'),
+                  path('',
+                       include('frontend.urls', namespace='frontend')),
+                  path('i18n/',
+                       include('django.conf.urls.i18n')),
+                  path('__debug__/',
+                       include(debug_toolbar.urls)),
+              ] + static(settings.MEDIA_URL,
+                         document_root=settings.MEDIA_ROOT)
