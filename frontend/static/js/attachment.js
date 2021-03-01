23f580a0c7a24eedd6405b001b5f3d04fbad52e6
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-03-01 19:17:11 +0100

    Update attachment js
    Separated html from js code

---
+++ frontend/static/js/attachment.js
@@ -0,0 +1,99 @@
+// Max attachments
+const MAX_ATTACHMENT = 20;
+
+// Restore deleted values
+const STACK = [];
+
+const ADD_BUTTON = document.getElementById("add-item-button")
+const REMOVE_BUTTON = document.getElementById("remove-item-button")
+
+// Number of children
+const CHILDREN = $('#items-form-container').children().length;
+
+// Beginning Visibility
+ADD_BUTTON.style.visibility = CHILDREN < MAX_ATTACHMENT ? "visible" : "hidden";
+REMOVE_BUTTON.style.visibility = CHILDREN === 0 ? "hidden" : "visible";
+
+
+/**
+ * Adds an attachment form to the current attachment div. Additionally
+ * update the visibility of the add and remove button depending on the
+ * number of visible attachment forms.
+ *
+ * @param event event
+ */
+function addAttachment(event) {
+    event.preventDefault();
+
+    REMOVE_BUTTON.style.visibility = "visible";
+
+    // Number of children
+    const children = $('#items-form-container').children().length;
+
+    // Update visibility of add attachment button
+    if (children + 1 === MAX_ATTACHMENT) {
+        // After this operation we increased the number of children by one
+        ADD_BUTTON.style.visibility = "hidden";
+    }
+
+    // Add an image attachment form
+    if (MAX_ATTACHMENT !== children) {
+        const tmplMarkup = $('#item-template').html();
+        const compiledTmpl = tmplMarkup.replace(/__prefix__/g, children);
+
+        // Add form
+        $('div#items-form-container').append(compiledTmpl);
+
+        if (STACK.length > 0) {
+            const value = STACK.pop();
+            const id = document.getElementById('id_form-' + children + '-id');
+            id.setAttribute('value', value);
+        }
+
+        // Restore value
+        if (STACK.length > 0) {
+            const id = document.getElementById('id_form-' + (children - 1) + '-id');
+            id.setAttribute('value', STACK.pop());
+        }
+
+        // Update form count
+        $('#id_form-TOTAL_FORMS').attr('value', children + 1);
+    }
+}
+
+/**
+ * Removes an attachment form to the current attachment div. Additionally
+ * update the visibility of the add and remove button depending on the
+ * number of visible attachment forms.
+ * @param event event
+ */
+function removeAttachment(event) {
+    event.preventDefault();
+
+    ADD_BUTTON.style.visibility = "visible";
+
+    // Number of children
+    const children = $('#items-form-container').children().length;
+
+    // Update visibility of add attachment button
+    if (children - 1 === 0) {
+        // After this operation we decreased the number of children by one
+        REMOVE_BUTTON.style.visibility = "hidden";
+    }
+
+    // Revert option
+    const id = document.getElementById('id_form-' + (children - 1) + '-id');
+    const value = id.getAttribute('value');
+
+    if (value !== null) {
+        STACK.push(value);
+    }
+
+    // Remove last child
+    const container = document.getElementById('items-form-container');
+
+    container.removeChild(container.lastElementChild);
+
+    // Update form count
+    $('#id_form-TOTAL_FORMS').attr('value', children - 1);
+}
