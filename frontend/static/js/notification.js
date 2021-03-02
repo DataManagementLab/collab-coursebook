24f6f734ab9374b2115f7a69c78386b3eb1eef12
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-03-02 18:06:50 +0100

    Modularization of fade in/out notifcation
    The notification is now in messages.html and each time a message will be displayed or we can invoke one manually, the message will fade in and after a timeout it will fade out

--- frontend/static/js/notification.js
+++ frontend/static/js/notification.js
@@ -12,14 +12,24 @@ function hideNotification() {
 }
 
 /**
- * Shows the notification element with the given message.
+ * Shows the notification element with the given message. Additionally we can add a class style
+ * to the notification which will be added and removed after this call.
+ *
  * @param message message of the notification
+ * @param clazz optional class style to be added
  */
-function showNotification(message) {
-    document.getElementById('notification-message').innerText = message;
+function showNotification(message, clazz = "") {
+    const parent = document.getElementById('notification');
+    const element = document.getElementById('notification-message');
+
+    parent.classList.add(clazz)
+    element.innerText = message;
+
     $('#notification').fadeIn();
     setTimeout(function () {
             $('#notification').fadeOut();
+            parent.classList.remove(clazz);
+            element.innerText = "";
         }, TIMEOUT
     );
 }
