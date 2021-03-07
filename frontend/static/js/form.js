2aa8d6a91f64847f164d88f09ab2139293aa0c5b
Nyanyan <46193012+Zentox@users.noreply.github.com>
2021-03-07 16:20:11 +0100

    Hotfix: required asterisk for attachment
    Minor bug - image field contains 2 asterisk

--- frontend/static/js/form.js
+++ frontend/static/js/form.js
@@ -10,6 +10,30 @@ function markRequired(clazz) {
             if (child.hasAttribute('required')) {
                 element.classList.add('form-required');
             }
+            if (child.children !== null && seekRequired(child)) {
+                element.classList.add('form-required');
+            }
         }
     }
+}
+
+/**
+ * Recursively seek an required child element if there exists one.
+ *
+ * @param obj element to check
+ * @return {boolean|*} true if element contains a required field
+ */
+function seekRequired(obj) {
+    const elements = obj.children;
+    for (let element of elements) {
+        for (let child of element.children) {
+            if (child.hasAttribute('required')) {
+                return true;
+            }
+            if (child.children !== null) {
+                return seekRequired(child);
+            }
+        }
+    }
+    return false;
 }
