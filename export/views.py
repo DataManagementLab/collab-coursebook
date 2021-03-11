c20fa4938ef18fa38387a27d4536b653c2ebb6c3
Niklas Heidler <niklas.heidler@hotmail.de>
2021-03-11 11:22:32 +0100

    docu update
    - updated the documentation in export/views.py
    - changed method names for better understanding

--- export/views.py
+++ export/views.py
@@ -11,7 +11,7 @@ from base.models import Course, Favorite, Content
 from export.helper_functions import Latex
 
 
-def generate_coursebook(request, pk, exp_all,  # pylint: disable=invalid-name
+def pdf_compile(request, pk, exp_all,  # pylint: disable=invalid-name
                         template="content/export/base.tex",
                         context=None):
     """Generate course book
@@ -68,6 +68,7 @@ def generate_coursebook_response(request, pk, file_name=_("Coursebook")):  # pyl
     """Generate course book response
 
     Generates a PDF file with name tags for students in the queryset and sends it to the browser.
+    This method generates a pdf file for the coursebook content (coursebook export)
 
     :param request: The given request
     :type request: WSGIRequest
@@ -81,17 +82,28 @@ def generate_coursebook_response(request, pk, file_name=_("Coursebook")):  # pyl
     """
 
     # Call the method for coursebook generation and write the output afterwards
-    (pdf, pdflatex_output, tex_template) = generate_coursebook(request, pk, False)
+    (pdf, pdflatex_output, tex_template) = pdf_compile(request, pk, False)
     return write_response(request, pdf, pdflatex_output, tex_template, file_name + ".pdf")
 
 
 def generate_course_export_response(request, pk, file_name=_("Course_Export")):  # pylint: disable=invalid-name
     """
-    <TODO: Iteration 6>
+    Generates a PDF file with name tags for students in the queryset and sends it to the browser.
+    This method generates a PDF for all contents in a course (course export)
+
+    :param request: The given request
+    :type request: WSGIRequest
+    :param pk: The primary key of the course
+    :type pk: int
+    :param file_name: The name of the file
+    :type file_name: str
+
+    :return: the http response of the generated PDF file
+    :rtype: HttpResponse
     """
 
     # Call the method for coursebook generation and write the output afterwards
-    (pdf, pdflatex_output, tex_template) = generate_coursebook(request, pk, True)
+    (pdf, pdflatex_output, tex_template) = pdf_compile(request, pk, True)
     return write_response(request, pdf, pdflatex_output, tex_template, file_name + ".pdf")
 
 
@@ -128,10 +140,11 @@ def write_response(request, pdf, pdflatex_output, tex_template, filename,
     return response
 
 
-def generate_pdf(user, content, template="content/export/base.tex", context=None):
+def generate_pdf_from_latex(user, content, template="content/export/base.tex", context=None):
     """Generate PDF
 
     Generates a PDF file with name tags for students in the queryset.
+    This method is used to compile a specific latex content into a PDF.
 
     :param user: The user of the content
     :type user: User
@@ -163,6 +176,7 @@ def generate_pdf_response(user, content):
     """Generate pdf response
 
     Generates a PDF file with name tags for students in the queryset.
+    This method compiles a specific latex content into a PDF.
 
     :param user: The user of the content
     :type user: User
@@ -174,5 +188,5 @@ def generate_pdf_response(user, content):
     """
 
     # Calls the function for generating the pdf and return the pdf
-    pdf = generate_pdf(user, content)
+    pdf = generate_pdf_from_latex(user, content)
     return pdf[0]
