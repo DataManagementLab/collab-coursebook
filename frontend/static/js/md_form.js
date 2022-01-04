e5a5a94f351c75f13cf7c905c278ff2c6d522535
Long_Nguyen <snowreshiram@gmail.com>
2022-01-04 17:59:04 +0100

    cleanup

--- frontend/static/js/md_form.js
+++ frontend/static/js/md_form.js
@@ -26,23 +26,9 @@ function updateOptionsState() {
     }
 }
 
-function disable(button, field) {
-    button.attr("style","opacity:0.5");
-    button.parent().removeClass('form-required')
-    field.setAttribute('disabled',"true");
-    field.removeAttribute('required');
-}
-
-function enable(button, field) {
-    button.attr("style","opacity:1.0");
-    button.parent().addClass('form-required')
-    field.removeAttribute('disabled');
-    field.setAttribute("style","opacity:1.0");
-    field.setAttribute('required','');
-}
-
 FILE_OPTIONS_BUTTON.addEventListener('click',updateOptionsState);
 TEXT_OPTIONS_BUTTON.addEventListener('click',updateOptionsState);
+
 updateOptionsState();
 
 
