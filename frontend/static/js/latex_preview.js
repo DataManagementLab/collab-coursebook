1d91a7bd3ab6097438da859e9797636d4e5064c3
Long_Nguyen <snowreshiram@gmail.com>
2022-01-30 11:45:43 +0100

    added js file

---
+++ frontend/static/js/latex_preview.js
@@ -0,0 +1,90 @@
+// URL of the latest PDF for previewing
+let currentURL;
+/**
+ * Submits the necessary data to the server requesting a preview PDF of the
+ * LaTeX content.
+ * If the request is successful, the PDF sent from the server is saved into
+ * memory and given an URL for access.
+ * If the request is unsuccessful, the server sends back a response containing
+ * the reason for failure.
+ *
+ * @param args data for the Ajax request
+ */
+function sendPreviewRequest(args) {
+    // Get preview frame
+    const previewFrame = document.getElementById("preview_frame");
+    // Remove visibility of the preview frame every time a preview request is sent
+    previewFrame.setAttribute("style","display:none")
+    // Get form data
+    const form = document.getElementsByClassName("post-form")[0];
+    const formData = new FormData(form);
+    // Add key to the form to indicate a preview request is sent
+    formData.set('latex-preview', true);
+    // Delete csrf token; for each call of the function a new csrf token must be generated
+    formData.delete('csrfmiddlewaretoken');
+    // Get CSRF token
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        }
+    });
+    // Ajax request
+    $.ajax({
+        xhrFields: {responseType: 'blob'},
+        url: args.url,
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (data, status, jqxhr) {
+            reason = jqxhr.statusText;
+            if (reason == "OK") {
+                data.name = "preview.pdf";
+                newURL = URL.createObjectURL(data);
+                // Destroy old preview before generating url for new preview, if an old preview exists
+                if (currentURL != null)
+                    URL.revokeObjectURL(currentURL);
+                // Update url in the preview frame
+                previewFrame.setAttribute("src", newURL)
+                currentURL = newURL;
+                const message = gettext("Preview successfully generated.");
+                showNotification(message, "alert-info");
+                // Return visibility for preview frame
+                previewFrame.removeAttribute("style");
+            }
+            else {
+                const message = gettext("Failed to generate preview - reason: " + reason + ".");
+                showNotification(message, "alert-danger");
+            }
+        },
+        error: function (data) {
+            const message = gettext("Error during data transfer to the server - status: %s");
+            showNotification(interpolate(message, [data.status]), "alert-danger");
+        },
+    });
+}
+
+/**
+ * Initializes the frame used for previewing LaTeX content.
+ *
+ * @param args attributes used to customize the preview frame
+ */
+function initPreviewFrame(args) {
+    previewFrame = document.createElement('embed');
+    // Set attributes for preview frame
+    attributes = Object.keys(args)
+    for (let idx in attributes) {
+        attr = attributes[idx];
+        previewFrame.setAttribute(attr, args[attr]);
+    }
+    // Constrain id and embedding type regardless of args
+    previewFrame.setAttribute('id', 'preview_frame');
+    previewFrame.setAttribute('type', 'application/pdf');
+    // Add preview frame to form
+    $('label[for="id_textfield"]').parent().after('<div class="row"><div class="col">'
+                                                   + previewFrame.outerHTML +
+                                                  '</div></div>');
+}
