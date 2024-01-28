a1238a114172d28c6b7a42ee79335d20089e01e7
Falk Beisecker <falkbeisecker@gmail.com>
2024-01-28 15:42:33 +0100

    Add test for Panopto Format Seconds

--- test/frontend/templatetags/tests_cc_frontend_tags.py
+++ test/frontend/templatetags/tests_cc_frontend_tags.py
@@ -3,9 +3,10 @@
 This file contains the test cases for /export/templatetags/cc_export_tags.py
 """
 
-from django.test import TestCase
-
 from frontend.templatetags.cc_frontend_tags import js_escape
+from datetime import timedelta
+from django.test import TestCase
+from frontend.templatetags.cc_frontend_tags import format_seconds
 
 
 class JSEscapeTestCase(TestCase):
@@ -25,3 +26,27 @@ class JSEscapeTestCase(TestCase):
         self.assertEqual('\\\\Hello World', escaped)
         escaped = js_escape('\\Hello\nWorld')
         self.assertEqual('\\\\Hello\\nWorld', escaped)
+
+
+
+class FormatSecondsTestCase(TestCase):
+    def test_format_seconds_numeric(self):
+        # Test converting seconds as a simple numeric string
+        seconds = "3600"  # 1 hour
+        expected_result = "1:00:00"
+        result = format_seconds(seconds)
+        self.assertEqual(result, expected_result)
+
+    def test_format_seconds_format(self):
+        # Test when seconds are already in the format "0:00:00"
+        seconds = "1:30:00"  # 1 hour and 30 minutes
+        expected_result = "1:30:00"
+        result = format_seconds(seconds)
+        self.assertEqual(result, expected_result)
+
+    def test_format_seconds_invalid(self):
+        # Test when seconds cannot be converted to an integer
+        seconds = "invalid"
+        expected_result = "invalid"
+        result = format_seconds(seconds)
+        self.assertEqual(result, expected_result)
