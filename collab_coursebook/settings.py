37fdb5f314c377665c5ba1b444da5a6d1a97d0dd
Falk Beisecker <falkbeisecker@gmail.com>
2024-03-10 18:18:46 +0100

    Fixed missspelled sentence

--- collab_coursebook/settings.py
+++ collab_coursebook/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = '_-xt(rpt+&&-c_^5h1bxgx(n63uctron!gi*k)*=4$rgu@nfs!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
